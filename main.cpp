6dbe954f158ee1837041318d33913072a6790a32
cyrusknopf <cyrus.knopf@turing.bio>
2025-07-25 12:59:04 +0100

    gitignore, correct CMAKE

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include "TuringClient.h"
+
 int main () {
     std::cout << "hello world" << std::endl;
+    using namespace turingClient;
+    TuringClient client("localhost");
 }
