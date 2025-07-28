42dc72e2ab6896ea4d55aa44c297fc6f0b8cb444
cyrusknopf <cyrus.knopf@turing.bio>
2025-07-28 17:02:41 +0100

    Client format

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-#include <iostream>
+#include <stdexcept>
 
 #include "TuringClient.h"
 
-
-
 int main () {
-    std::cout << "hello world" << std::endl;
     using namespace turingClient;
-    TuringClient client("localhost");
+    using Col = std::vector<std::unique_ptr<TypedColumn>>;
+    
+    TuringClient client("http://127.0.0.1:6666");
+
+    Col ret{};
+    bool res = client.query("match (n) return n", "simpledb", ret);
+
+    if (!res) {
+        throw std::runtime_error("Failed to query");
+    }
 }
