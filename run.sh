b3422b01eaf44243b95ac5ebdccbb0533c886586
maximevincent <maxime.vincent@turing.bio>
2026-02-02 16:06:50 +0000

    Add database parameter and use var env for path

--- run.sh
+++ run.sh
@@ -4,9 +4,11 @@ set -e
 shopt -s expand_aliases
 
 GIT_ROOT="$(git rev-parse --show-toplevel)"
+source "$GIT_ROOT/env.sh"
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$script_dir/servers_python"
+DATASET=${1-simpledb}
+
+cd $SCRIPTS
 alias uvrun="uv run --directory $GIT_ROOT python -m turingbench"
 
 uv run python manage_servers.py turingdb stop > /dev/null || true
@@ -15,15 +17,15 @@ uv run python manage_servers.py memgraph stop > /dev/null || true
 
 # TuringDB benchmark
 uv run python manage_servers.py turingdb start || true
-uvrun turingdb --query-file '/home/dev/work/turing-bench/sample_queries/simpledb/labelsets.cypher'
+uvrun turingdb --query-file "$QUERIES_DIR/$DATASET/labelsets.cypher"
 uv run python manage_servers.py turingdb stop
 
 # Neo4j benchmark
 uv run python manage_servers.py neo4j start
-uvrun neo4j --query-file '/home/dev/work/turing-bench/sample_queries/simpledb/labelsets.cypher'
+uvrun neo4j --query-file "$QUERIES_DIR/$DATASET/labelsets.cypher"
 uv run python manage_servers.py neo4j stop
 
 # Memgraph benchmark
 uv run python manage_servers.py memgraph start
-uvrun memgraph --query-file '/home/dev/work/turing-bench/sample_queries/simpledb/labelsets.cypher' --database=memgraph
+uvrun memgraph --query-file "$QUERIES_DIR/$DATASET/labelsets.cypher" --database=memgraph
 uv run python manage_servers.py memgraph stop
