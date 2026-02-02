e8863944947aa78bf6f1f9ae3a8c4ae0f19eae47
luclabarriere <luc.labarriere@turing.bio>
2026-02-02 11:02:26 +0000

    Prepared script to parse the json

---
+++ cypher-to-turingdb-cypher.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str, metavar="input-file")
+    parser.add_argument("--output-file", type=str, default="/tmp/output.tdbcypher")
+    args = parser.parse_args()
+
+    input_file = args.input_file
+    output_file = args.output_file
+
+    node_count = 0
+    with open(args.input_file, "r") as inp:
+        with open(args.output_file, "w") as out:
+            out.write("CREATE\n")
+
+            i = 0
+            for line in inp:
+                line = line.strip()
+                print(line)
+                break
+
+
+                if i > 200:
+                    break
+
+                i += 1
