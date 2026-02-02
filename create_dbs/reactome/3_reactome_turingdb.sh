14dc51234ff602293bf4506e6a39910e1add13b4
maximevincent <maxime.vincent@turing.bio>
2026-02-02 14:32:40 +0000

    Add script to create reactome in TuringDB

--- create_dbs/reactome/3_reactome_turingdb.sh
+++ create_dbs/reactome/3_reactome_turingdb.sh
@@ -6,16 +6,14 @@ shopt -s expand_aliases
 REPO_ROOT=$(git rev-parse --show-toplevel)
 source "$REPO_ROOT/env.sh"
 
-DUMP_FILE="simpledb-4.3.dump"  # "reactome.dump"
-DUMP_PATH="$NEO4J_IMPORT/$DUMP_FILE"
+JSON_FILE=/tmp/output.json
+TDB_CYPHER_FILE=simpledb.tbdcypher  # reactome.tbdcypher
 
 # Check if TURING_HOME is set
 if [ -z "$TURING_HOME" ]; then
     echo "TURING_HOME is not set. Please set it to the TuringDB installation directory."
     exit 1
 fi
-
-tdb="uv run turingdb"  # "$TURING_HOME/bin/turingdb"
 
 # Load reactome in turingdb
 if [ -d "$TURINGDB_DIR" ]; then
@@ -24,10 +22,12 @@ if [ -d "$TURINGDB_DIR" ]; then
     rm -rf "$TURINGDB_DIR"
 fi
 
-mkdir -p "$TURINGDB_DIR/data"
-mkdir -p "$TURINGDB_DIR/graphs"
-cp $DUMP_PATH "$TURINGDB_DIR/data/$DUMP_FILE"
+mkdir -p $TURINGDB_DATA_DIR
+mkdir -p $TURINGDB_GRAPHS_DIR
 
-echo "Loading Reactome dump into turingdb..."
-# Run in truly clean environment - only preserve HOME and basic shell info
-env -i HOME="$HOME" TURING_HOME=$TURING_HOME bash -l -c "echo 'LOAD NEO4J '\''$DUMP_FILE'\'' AS reactome' | uv run turingdb -turing-dir '$TURINGDB_DIR'"
+echo "Loading Reactome json into turingdb..."
+uv run "$SCRIPTS/cypher-to-turingdb-cypher.py" $JSON_FILE --output-file "$TURINGDB_DATA_DIR/$TDB_CYPHER_FILE"
+
+# Create TuringDB graph using generated cypher script
+uv run turingdb -turing-dir $TURINGDB_DIR < "$TURINGDB_DATA_DIR/$TDB_CYPHER_FILE"
+echo "Reactome successfully created in TuringDB."
