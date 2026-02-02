726f164ee3bd62c8c82c9f31005177c527c8ad48
maximevincent <maxime.vincent@turing.bio>
2026-02-02 09:17:40 +0000

    Add copy from s3 bucket + example with simpledb for now

--- create_dbs/reactome/0_download_reactome.sh
+++ create_dbs/reactome/0_download_reactome.sh
@@ -5,13 +5,15 @@ set -e
 REPO_ROOT=$(git rev-parse --show-toplevel)
 source "$REPO_ROOT/env.sh"
 
-DUMP_FILE="$NEO4J_IMPORT/reactome.dump"
-DUMP_URL="https://reactome.org/download/current/reactome.graphdb.dump"
+DUMP_FILE="simpledb-4.3.dump"  # "reactome.dump"
+DUMP_PATH="$NEO4J_IMPORT/$DUMP_FILE"
+DUMP_URL="s3://turing-internal/simpledb-4.3.dump"  # "https://reactome.org/download/current/reactome.graphdb.dump"
 
 # Download dump if not exists
-if [ ! -f "$DUMP_FILE" ]; then
+if [ ! -f $DUMP_PATH ]; then
     echo "Downloading Reactome dump..."
-    wget -O "$DUMP_FILE" "$DUMP_URL"
+    # wget -O "$DUMP_PATH" "$DUMP_URL"
+    aws s3 cp $DUMP_URL $DUMP_PATH
     echo "Reactome dump downloaded."
 else
     echo "Reactome dump already exists at $DUMP_FILE"
