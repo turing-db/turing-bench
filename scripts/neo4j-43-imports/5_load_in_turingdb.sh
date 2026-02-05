59d94d07168222faead56d7c8e7654022fc7c2a6
luclabarriere <luc.labarriere@turing.bio>
2026-02-05 09:45:38 +0000

    Cleaned up scripts to allow to use them on any neo4j 4.3 dump

---
+++ scripts/neo4j-43-imports/5_load_in_turingdb.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+shopt -s expand_aliases
+REPO_ROOT=$(git rev-parse --show-toplevel)
+source "$REPO_ROOT/env.sh"
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <dataset>"
+    exit 1
+fi
+
+DATASET=$1
+JSON_FILE="$DUMPS/$DATASET.jsonl"
+SAVE_PATH="$DUMPS/$DATASET.turingdb"
+
+if [ -d "$SAVE_PATH" ]; then
+    echo "- $DATASET dump already exists in $SAVE_PATH. Skipping..."
+    exit 1
+fi
+
+echo "- Loading $DATASET jsonl into turingdb..."
+mkdir -p $SAVE_PATH/data
+cp "$JSON_FILE" "$SAVE_PATH/data/output.json"
+
+start=$(date +%s)
+echo "LOAD JSONL 'output.json' AS $DATASET;" | turingdb -turing-dir $SAVE_PATH
+echo "- Loading $DATASET jsonl into turingdb took $(elapsed $start)"
+
